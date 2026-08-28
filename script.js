38acef6eb8bec9a8b95a73854fee979e532ef29c
saakshi sharma <saakshi8sharma@gmail.com>
2026-08-28 12:23:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+const button = document.getElementById("subscribe");
+const emailInput = document.getElementById("email");
+const message = document.getElementById("message");
 
+button.addEventListener("click", async () => {
+
+    const email = emailInput.value;
+
+    const response = await fetch("/subscribe", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            email: email
+        })
+    });
+
+    console.log("HTTP Status Code:", response.status);
+    const data = await response.json();
+    console.log("Server Response:", data);
+    
+    message.textContent = data.message;
+     if (response.ok) {
+        emailInput.value = "";
+     }
+});
